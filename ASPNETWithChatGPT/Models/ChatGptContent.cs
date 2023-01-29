d0345a89fad5cfa7952c05a7dac8b83f23470a32
Temurkhann <109779514+Temurkhann@users.noreply.github.com>
2023-01-29 10:23:17 +0500

    Menu bar Edit changed to Tools

--- ASPNETWithChatGPT/Models/ChatGptContent.cs
+++ ASPNETWithChatGPT/Models/ChatGptContent.cs
@@ -12,6 +12,7 @@ namespace ASPNETWithChatGPT.Models
         public string Text { get; set; }
         public string Index { get; set; }
         public string Logprobs { get; set; }
+
         [JsonProperty("finish_reason")]
         public string FinishReason { get; set; }
     }
